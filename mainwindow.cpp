ac687dfb5aae953015d100e45113ed85c4cd9fc3
coucou1121 <sebastien.blessemaille@heig-vd.ch>
2017-04-02 22:47:00 +0200

    test color green button

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,100 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //set the color of the main windows
+    MainSetColor();
+
+    //set the color of push button
+    BPSetColor();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::MainSetColor(void)
+{
+    setStyleSheet("background-color: rgb(170, 170, 170);");
+}
+
+void MainWindow::BPSetColor(void)
+{
+    ui->BP_Green->setStyleSheet("background-color:rgba(255, 255, 127, 50);"
+                                "image: url(:/images/Images/folder.png);");
+
+
+//    ui->BP_Green->setStyleSheet("background-color: "
+//                                "qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 "
+//                                "rgba(0, 0, 0, 255), stop:0.55 "
+//                                "rgba(235, 148, 61, 255), stop:0.98 "
+//                                "rgba(0, 0, 0, 255), stop:1 "
+//                                "rgba(0, 0, 0, 0));");
+//    ui->BP_Green->setStyleSheet("background-image: url(:/images/Images/folder.png);");
+}
+
+void MainWindow::on_BP_Green_released()
+{
+
+//    _checked = _checked == true ? false : true;
+//    if(_checked)
+//    {
+//        ui->BP_Green->setStyleSheet("background-color: "
+//                                    "qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 "
+//                                    "rgba(0, 0, 0, 255), stop:0.55 "
+//                                    "rgba(77, 225, 124, 255), stop:0.98 "
+//                                    "rgba(0, 0, 0, 255), stop:1 "
+//                                    "rgba(0, 0, 0, 0));");
+//    }
+//    else
+//    {
+//        ui->BP_Green->setStyleSheet("background-color: "
+//                                    "qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 "
+//                                    "rgba(0, 0, 0, 255), stop:0.55 "
+//                                    "rgba(235, 148, 61, 255), stop:0.98 "
+//                                    "rgba(0, 0, 0, 255), stop:1 "
+//                                    "rgba(0, 0, 0, 0));");
+//    }
+}
+
+void MainWindow::on_BP_Green_toggled(bool _checked)
+{
+//    if(_checked)
+//    {
+//        ui->BP_Green->setStyleSheet("background-color: red");
+////                                    "qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 "
+////                                    "rgba(0, 0, 0, 255), stop:0.55 "
+////                                    "rgba(77, 225, 124, 255), stop:0.98 "
+////                                    "rgba(0, 0, 0, 255), stop:1 "
+////                                    "rgba(0, 0, 0, 0));");
+//    }
+//    else
+//    {
+//        ui->BP_Green->setStyleSheet("background-color: green");
+////                                    "qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 "
+////                                    "rgba(0, 0, 0, 255), stop:0.55 "
+////                                    "rgba(235, 148, 61, 255), stop:0.98 "
+////                                    "rgba(0, 0, 0, 255), stop:1 "
+////                                    "rgba(0, 0, 0, 0));");
+//    }
+}
+
+void MainWindow::on_BP_Green_clicked()
+{
+    // _checked = _checked == true ? false : true;
+}
+
+void MainWindow::on_BP_Green_pressed()
+{
+     _checked = _checked == true ? false : true;
+    if(ui->BP_Green->isChecked())
+    {
+        ui->BP_Green->setStyleSheet("background-color:rgba(100, 0, 0, 50);"
+                                    "image: url(:/images/Images/folder.png);");
+    }
+    else
+    {
+        ui->BP_Green->setStyleSheet("background-color:rgba(255, 255, 127, 50);"
+                                    "image: url(:/images/Images/folder.png);");
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,8 +15,21 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+
+    void on_BP_Green_released();
+
+    void on_BP_Green_toggled(bool _checked);
+
+    void on_BP_Green_clicked();
+
+    void on_BP_Green_pressed();
+
 private:
+    bool _checked = false;
     Ui::MainWindow *ui;
+    void BPSetColor(void);
+    void MainSetColor(void);
 };
 
 #endif // MAINWINDOW_H
